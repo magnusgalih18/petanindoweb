c5063536e476b7570ecddd12d1c06296a28bc14b
magnusgalih18 <magnus.galih@gmail.com>
2021-01-06 12:01:04 +0700

    update

--- resources/views/auth/registerSeller.blade.php
+++ resources/views/auth/registerSeller.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-  <title>Register &mdash; Stisla</title>
+  <title>Register Seller | Petanindo</title>
 
   <!-- General CSS Files -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -30,7 +30,7 @@
 
             <div class="card card-primary">
               <div class="card-header"><h4>Register</h4></div>
-                
+
               <div class="card-body">
                 @if($errors->any())
                   @foreach($errors->all() as $error)
